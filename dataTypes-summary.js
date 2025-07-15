c5329aa3f055ba481a2c43d6b4c0b1a790876d45
Arshiya <arshiyazade@gmail.com>
2025-07-15 03:00:57 +0000

    dataTypes-summary

---
+++ dataTypes-summary.js
@@ -0,0 +1,62 @@
+// Primitives
+
+// 7 types: String , Number , Boolean , NUll , undefined ,Symbol , BigInt
+
+
+// References ( Non Primirtives)
+
+//Array , Objects, Functions
+
+// Javascript is dynamically typed language means type checking occurs at run time no explicit declaration
+
+const score = 100;
+const scoreValue = 100.34;
+
+const isLogged = false;
+const temp = null;
+console.log(typeof temp); //object
+
+let userEmail; //undefined
+
+const id = Symbol('123');
+const id2 = Symbol('123');
+console.log(id === id2); //false
+
+// Reference (Non primitives)
+// Array , Objects , Functions
+
+const num = [1,2,3,4,5,6];
+
+let myObj = {
+  name : "arshiya",
+  age : 21,
+  gen : "female"
+}
+
+const myFunction = function(){
+  {
+    var name = "arshiya";
+    let name2 = "anas";
+    console.log(name2);
+  }
+  console.log(name);
+}
+
+
+myFunction();
+console.log(typeof myFunction);
+
+const bigNum = 1234455666609476890573690n;
+console.log(typeof bigNum);
+
+
+//Type of value      =>   Results
+// undefined  => "undefined"
+// Null       => "object"
+// Boolean     => "boolean"
+// Number      => "number"
+// BigInt      => "bigint"
+// String      => "string"
+// Object      => "object"
+// Function call => "function"
+
